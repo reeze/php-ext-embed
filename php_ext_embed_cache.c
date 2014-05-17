b15a7c330215186e81593523a65d4f115f9e7c2c
Shen Jiale <jerryshen.zju@gmail.com>
2014-05-17 17:33:07 +0800

    Local iterator do not affect global variable

--- php_ext_embed_cache.c
+++ php_ext_embed_cache.c
@@ -34,32 +34,32 @@ static HashTable *orig_eg_class_table;
 
 void php_embed_cache_restore(TSRMLS_D)
 {
-	function_table_tail = function_table_tail ? function_table_tail->pListNext
-						: embed_global_function_table.pListHead;
-	class_table_tail    = class_table_tail ? class_table_tail->pListNext
-						: embed_global_class_table.pListHead;
+	Bucket *function_table_iter = function_table_tail ?
+		function_table_tail->pListNext : embed_global_function_table.pListHead;
+	Bucket *class_table_iter    = class_table_tail ?
+		class_table_tail->pListNext : embed_global_class_table.pListHead;
 
-	while (function_table_tail != NULL) {
-		zend_op_array *op_array = (zend_op_array*)function_table_tail->pData;
+	while (function_table_iter != NULL) {
+		zend_op_array *op_array = (zend_op_array*)function_table_iter->pData;
 		if (op_array->type == ZEND_USER_FUNCTION) {
-			if (zend_hash_add(CG(function_table), function_table_tail->arKey,
-					function_table_tail->nKeyLength, op_array, sizeof(zend_op_array),
-					NULL) == FAILURE && function_table_tail->arKey[0] != '\0') {
+			if (zend_hash_add(CG(function_table), function_table_iter->arKey,
+					function_table_iter->nKeyLength, op_array, sizeof(zend_op_array),
+					NULL) == FAILURE && function_table_iter->arKey[0] != '\0') {
 				CG(in_compilation) = 1;
 				CG(compiled_filename) = (char *)op_array->filename;
 				CG(zend_lineno) = op_array->line_start;
 				zend_error(E_ERROR, "Cannot redeclare function %s()", op_array->function_name);
 			}
 		}
-		function_table_tail = function_table_tail->pListNext;
+		function_table_iter = function_table_iter->pListNext;
 	}
 
-	while (class_table_tail != NULL) {
-		zend_class_entry **ce = (zend_class_entry**)class_table_tail->pData;
+	while (class_table_iter != NULL) {
+		zend_class_entry **ce = (zend_class_entry**)class_table_iter->pData;
 		if ((*ce)->type == ZEND_USER_CLASS) {
-			if (zend_hash_add(CG(class_table), class_table_tail->arKey,
-					class_table_tail->nKeyLength, ce, sizeof(zend_class_entry*),
-					NULL) == FAILURE && class_table_tail->arKey[0] != '\0') {
+			if (zend_hash_add(CG(class_table), class_table_iter->arKey,
+					class_table_iter->nKeyLength, ce, sizeof(zend_class_entry*),
+					NULL) == FAILURE && class_table_iter->arKey[0] != '\0') {
 				CG(in_compilation) = 1;
 				#if PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION >= 4
 				CG(compiled_filename) = (char *)(*ce)->info.user.filename;
@@ -71,7 +71,7 @@ void php_embed_cache_restore(TSRMLS_D)
 				zend_error(E_ERROR, "Cannot redeclare class %s{}", (*ce)->name);
 			}
 		}
-		class_table_tail = class_table_tail->pListNext;
+		class_table_iter = class_table_iter->pListNext;
 	}
 }
 
